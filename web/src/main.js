a60e8e1ce6d730e5b064583c328203a78d44c67e
odan <odan3240@gmail.com>
2018-05-02 19:25:03 +0900

    add babel-polyfill

--- web/src/main.js
+++ web/src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import 'babel-polyfill';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
 import 'material-design-icons/iconfont/material-icons.css';
