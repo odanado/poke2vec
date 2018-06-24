e1d5816b23e97a0dd9e982d1030fb7d38b5e971f
Kita Tomoya <kita@mfac.jp>
2018-06-24 12:58:31 +0900

    2d visualizerにvgc2018を追加した

---
+++ web/src/components/SelectPoke2vecModel.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-select
+    :items="poke2vecModels"
+    v-model="selectedModel"
+    return-object
+    label="モデル"
+    class="selecte-poke2vec-model"
+    @change="changeModel"/>
+</template>
+
+<script>
+export default {
+  props: {
+    poke2vecModels: {
+      type: Array,
+      required: true,
+    },
+  },
+  data: () => ({
+    selectedModel: null,
+  }),
+  methods: {
+    changeModel(value) {
+      this.$emit('changeModel', value);
+    },
+  },
+};
+</script>
+<style scoped>
+.selecte-poke2vec-model {
+  white-space: nowrap
+}
+</style>
--- web/src/pages/Visualizer2d.vue
+++ web/src/pages/Visualizer2d.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <SelectPoke2vecModel
+      :poke2vec-models="poke2vecModels"
+      @changeModel="changePoke2vecModel"
+    />
     <v-layout>
       <v-slider
         v-model="canvasScaleFactor"
@@ -25,12 +29,21 @@
 <script>
 
 import Visualizer2dCanvas from '@/components/Visualizer2dCanvas';
+import SelectPoke2vecModel from '@/components/SelectPoke2vecModel';
 
-const poke2vec = require('@/data/pca_2d_gen7battlespotsingles_ns_64_poke2vec');
-
+/* eslint-disable global-require */
+const poke2vecs = new Map(Object.entries({
+  pca_2d_gen7battlespotsingles_ns_64: require('@/data/pca_2d_gen7battlespotsingles_ns_64_poke2vec.json'),
+  pca_2d_gen7vgc2018_ns_64: require('@/data/pca_2d_gen7vgc2018_ns_64_poke2vec.json'),
+}));
+/* eslint-enable global-require */
 export default {
+  created() {
+    this.selectedPoke2vecModel = this.poke2vecModels[1];
+  },
   components: {
     Visualizer2dCanvas,
+    SelectPoke2vecModel,
   },
   methods: {
     changeScaleFactor(scaleFactor) {
@@ -44,10 +57,28 @@ export default {
       this.$refs.canvas.reset();
       this.$refs.canvas.draw();
     },
+    changePoke2vecModel(model) {
+      this.selectedPoke2vecModel = model;
+      // FIXME: dataのupdateに少し時間がかかるため、直後にdrawを呼び出すと前のdataでdrawしてしまう
+      setInterval(this.$refs.canvas.draw, 100);
+    },
+  },
+  computed: {
+    poke2vec() {
+      const { value } = this.selectedPoke2vecModel;
+      if (poke2vecs.has(value)) {
+        return poke2vecs.get(value);
+      }
+      return [];
+    },
   },
   data: () => ({
-    poke2vec,
+    selectedPoke2vecModel: null,
     canvasScaleFactor: 1,
+    poke2vecModels: [
+      { text: 'VGC2018 64次元', value: 'pca_2d_gen7vgc2018_ns_64' },
+      { text: 'シングルバトル 2次元', value: 'pca_2d_gen7battlespotsingles_ns_64' },
+    ],
   }),
 };
 </script>
