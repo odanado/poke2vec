d641bf6f8ec6e3292e13f897da46b62d30792b2b
odan <odan3240@gmail.com>
2018-09-18 13:42:30 +0900

    path変更

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '@/pages/Home';
+// import Home from '@/pages/Home';
 import MostSimilar from '@/pages/MostSimilar';
 import Visualizer2d from '@/pages/Visualizer2d';
 
@@ -10,16 +10,11 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Home',
-      component: Home,
-    },
-    {
-      path: '/MostSimilar',
       name: 'MostSimilar',
       component: MostSimilar,
     },
     {
-      path: '/Visualizer2d',
+      path: '/visualizer-2d',
       name: '2D Visualizer',
       component: Visualizer2d,
     },
